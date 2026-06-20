dd2742671a755eb8783044829efb8770d5a48041
Khargosxh18 <nikhilyadav31071@gmail.com>
2026-06-20 08:04:03 +0000

    peridot: Switch to Official Build Setup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,12 +21,12 @@ fi
 #    echo "vendor/bcr already exists, skipping..."
 #fi
 
-## Viper4A
-#if [ ! -d "packages/apps/ViPER4AndroidFX" ]; then
-#    git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
-#else
-#    echo "packages/apps/ViPER4Android already exists, skipping..."
-#fi
+# Viper4A
+if [ ! -d "packages/apps/ViPER4AndroidFX" ]; then
+    git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
+else
+    echo "packages/apps/ViPER4Android already exists, skipping..."
+fi
 
 # Miui Camera
 if [ ! -d "device/xiaomi/peridot-miuicamera" ]; then
