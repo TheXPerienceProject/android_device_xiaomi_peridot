9cd637a9174581b267b626a5b317b8c7f614a76a
Khargosxh18 <nikhilyadav31071@gmail.com>
2026-08-22 08:27:31 +0000

    peridot: Build NotGameTurbo

--- device.mk
+++ device.mk
@@ -566,3 +566,6 @@ $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Vendor MiuiCamera
 $(call inherit-product-if-exists, device/xiaomi/peridot-miuicamera/device.mk)
+
+# NotGameTurbo
+$(call inherit-product, packages/apps/NotGameTurbo/notgameturbo.mk)
