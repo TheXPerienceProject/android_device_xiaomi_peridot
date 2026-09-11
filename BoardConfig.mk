Mohithash/device_xiaomi_peridot

peridot: Use legacy libion implementation
* At least one blob directly depends on this, with
  many other bits and pieces likely.

Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06

BoardConfig.mk
@@ -236,6 +236,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
+include device/lineage/sepolicy/libion/sepolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/xiaomi/peridot/sepolicy/SEPolicy-diag.mk

TheXPerienceProject/android_device_xiaomi_peridot

BoardConfig.mk
@@ -234,6 +234,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
 include device/xperience/sepolicy/libperfmgr/sepolicy.mk
+include device/xperience/sepolicy/libion/sepolicy.mk
+include device/xperience/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/xiaomi/peridot/sepolicy/SEPolicy-diag.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor